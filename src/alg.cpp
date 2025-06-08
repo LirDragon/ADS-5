9f32957c47417bc288e1e49aebccfc85d1255f41
LirDragon <arin_artist@mail.ru>
2025-06-08 19:17:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,69 @@
 #include "tstack.h"
 
 std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
+ TStack<char, 100> stack;
+    std::stringstream postfix;
+    bool prevWasDigit = false;
+    for (size_t i = 0; i < inf.size(); ++i) {
+        char c = inf[i];
+        if (isspace(c)) {
+            continue;
+        }
+        if (isdigit(c)) {
+            if (prevWasDigit) {
+                postfix << c; 
+            } else {
+                if (i != 0) postfix << ' ';
+                postfix << c;
+            }
+            prevWasDigit = true;
+        } 
+        else {
+            prevWasDigit = false;
+            if (c == '(') {
+                stack.push(c);
+            } 
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.top() != '(') {
+                    postfix << ' ' << stack.pop();
+                }
+                stack.pop();
+            } 
+            else {
+                while (!stack.isEmpty() && getPriority(stack.top()) >= getPriority(c)) {
+                    postfix << ' ' << stack.pop();
+                }
+                stack.push(c);
+            }
+        }
+    }
+    while (!stack.isEmpty()) {
+        postfix << ' ' << stack.pop();
+    }
+    return postfix.str();
+}
   return std::string("");
 }
-
 int eval(const std::string& pref) {
-  // добавьте код
+   TStack<int, 100> stack;
+    std::istringstream iss(post);
+    std::string token;
+    
+    while (iss >> token) {
+        if (isdigit(token[0])) {
+            stack.push(std::stoi(token));
+        } 
+        else {
+            int b = stack.pop();
+            int a = stack.pop();
+            switch(token[0]) {
+                case '+': stack.push(a + b); break;
+                case '-': stack.push(a - b); break;
+                case '*': stack.push(a * b); break;
+                case '/': stack.push(a / b); break;
+            }
+        }
+    }
+    return stack.pop();
   return 0;
 }
