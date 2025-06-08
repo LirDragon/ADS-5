13f8247b565439d326ed2cd023c2b398743cbc3d
LirDragon <arin_artist@mail.ru>
2025-06-08 19:10:46 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,31 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[SIZE];
+    int topIndex;
+    
+public:
+    TStack() : topIndex(-1) {}
+    
+    void push(const T& value) {
+        if (topIndex >= SIZE - 1) throw "Stack overflow";
+        data[++topIndex] = value;
+    }
+    
+    T pop() {
+        if (isEmpty()) throw "Stack underflow";
+        return data[topIndex--];
+    }
+    
+    T top() const {
+        if (isEmpty()) throw "Stack is empty";
+        return data[topIndex];
+    }
+    
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
